15d5284588c9829dfda569bb1bdf660c046ee096
Kemsha <arijan.komsic@stu.ibu.edu.ba>
2023-07-07 16:15:58 +0200

    services

---
+++ rest/services/BaseService.class.php
@@ -0,0 +1,26 @@
+<?php
+abstract class BaseService{
+
+    protected $dao;
+
+    public function __construct($dao){
+        $this->dao = $dao;
+    }
+
+    public function getAll(){
+        return $this->dao->getAll();
+    }
+
+    public function add($params){
+        return $this->dao->add($params);
+    }
+
+    public function update($params,$id){
+        return $this->dao->update($params,$id);
+    }
+    public funciton delete($id){
+        return $this->dao->delete($id);
+    }
+}
+
+?>
